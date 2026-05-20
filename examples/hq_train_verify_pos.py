69678a7b8f9d36ce77682f8591e67fd3a68d3115
van-dai-vuong <99916615+van-dai-vuong@users.noreply.github.com>
2026-05-20 10:48:12 -0400

    update hq_train

---
+++ examples/hq_train_verify_pos.py
@@ -0,0 +1,443 @@
+# Temporary import. It will be removed in the final vserion
+import os
+import sys
+
+# Add the 'build' directory to sys.path in one line
+sys.path.append(
+    os.path.normpath(os.path.join(os.path.dirname(__file__), "..", "build"))
+)
+from typing import Optional
+
+import fire
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+import pytagi
+import pytagi.metric as metric
+from examples.data_loader import TimeSeriesDataloader
+from pytagi import Normalizer as normalizer
+from pytagi import exponential_scheduler
+from pytagi.nn import LSTM, Linear, OutputUpdater, Sequential
+
+
+def main(num_epochs: int = 10, batch_size: int = 32, sigma_v: float = 1):
+    """Run training for time-series forecasting model"""
+    # Dataset
+    output_col = [0]
+    num_features = 2
+    input_seq_len = 52
+    output_seq_len = 1
+    seq_stride = 1
+
+    data_file = "./data/hq/ts_weekly_values_final.csv"
+    data_time = "./data/hq/ts_weekly_datetimes_final.csv"
+
+    cols = range(101)
+    # cols = range(96)
+    df = pd.read_csv(
+        data_file, skiprows=1, delimiter=",", header=None, usecols=cols
+    )
+    df_time = pd.read_csv(
+        data_time, skiprows=1, delimiter=",", header=None, usecols=cols
+    )
+    train_split = 0.7
+    val_split = 0.15
+
+    num_ts = df.shape[1]
+    num_iter = int(np.ceil(num_ts / batch_size))
+    time_covariates = ["week_of_year"]
+    rmse_optim = 10
+    epoch_optim = 0
+    patience = 15
+    warmup_epochs = 3
+
+    # # Data loader
+    train_dtl_dict = {}
+    val_dtl_dict = {}
+
+    for ts in range(num_ts):
+        df_temp = df.iloc[:, [ts]]
+        df_temp.index = pd.to_datetime(df_time.iloc[:, ts])
+
+        last_idx = df_temp.index.get_loc(df_temp.iloc[:, 0].last_valid_index())
+        train_end_idx = int(train_split * last_idx)
+        val_end_idx = int((train_split + val_split) * last_idx)
+        df_train_temp = df_temp.iloc[0:train_end_idx, :]
+
+        df_val_temp = df_temp.iloc[
+            train_end_idx - input_seq_len + 1 : val_end_idx, :
+        ]
+
+        train_dtl_dict[int(ts)] = TimeSeriesDataloader(
+            x_file="",
+            date_time_file="",
+            output_col=output_col,
+            input_seq_len=input_seq_len,
+            output_seq_len=output_seq_len,
+            num_features=num_features,
+            time_covariates=time_covariates,
+            keep_last_time_cov=True,
+            stride=seq_stride,
+            df=df_train_temp,
+        )
+
+        val_dtl_dict[int(ts)] = TimeSeriesDataloader(
+            x_file="",
+            date_time_file="",
+            output_col=output_col,
+            input_seq_len=input_seq_len,
+            output_seq_len=output_seq_len,
+            num_features=num_features,
+            stride=seq_stride,
+            df=df_val_temp,
+            x_mean=train_dtl_dict[int(ts)].x_mean,
+            x_std=train_dtl_dict[int(ts)].x_std,
+            time_covariates=time_covariates,
+            keep_last_time_cov=True,
+        )
+
+    # Network
+    input_size = input_seq_len + len(time_covariates)
+    pytagi.manual_seed(1)
+    net = Sequential(
+        LSTM(input_size, 50, False, 1),
+        LSTM(50, 50, True, 1),
+        Linear(50, 1),
+    )
+    # net.to_device("cuda")
+    # net.set_threads(1)  # multi-processing is slow on a small net
+    # net.input_state_update = True
+    out_updater = OutputUpdater(net.device)
+
+    # -------------------------------------------------------------------------#
+    # Training
+    pbar = tqdm(range(num_epochs), desc="Training Progress")
+    for epoch in pbar:
+
+        ts_list = np.random.permutation(num_ts)
+        sigma_v = exponential_scheduler(
+            curr_v=sigma_v, min_v=0.1, decaying_factor=0.8, curr_iter=epoch
+        )
+
+        var_y = np.full(
+            (batch_size * len(output_col),), sigma_v**2, dtype=np.float32
+        )
+
+        # Train
+        for itera in range(num_iter):
+            ts_batch = ts_list[itera * batch_size : (itera + 1) * batch_size]
+            ts_data = {}
+            num_sample_inter = 0
+
+            for ts in ts_batch:
+                ts_data[int(ts)] = train_dtl_dict[int(ts)].dataset["value"]
+                num_sample_inter = np.maximum(
+                    len(ts_data[int(ts)][1]), num_sample_inter
+                )
+
+            x, _ = prep_x_y(0, ts_batch, ts_data, input_size, batch_size)
+            x = x.reshape(batch_size, input_size)
+            var_x = np.zeros_like(x)
+
+            for t in range(num_sample_inter):
+
+                _x, y = prep_x_y(t, ts_batch, ts_data, input_size, batch_size)
+                _x = _x.reshape(batch_size, input_size)
+                x[:, -1] = _x[:, -1]
+
+                # Feed forward
+                m_pred, v_pred = net(x.flatten(), var_x.flatten())
+
+                # Update output layer
+                out_updater.update(
+                    output_states=net.output_z_buffer,
+                    mu_obs=y,
+                    var_obs=var_y,
+                    delta_states=net.input_delta_z_buffer,
+                )
+
+                # Feed backward
+                net.backward()
+                net.step()
+
+                # modify input
+                x[:, :-2] = x[:, 1:-1]
+                x[:, -2] = m_pred.flatten()
+                x = np.nan_to_num(x, nan=0.0)
+                var_x[:, :-2] = var_x[:, 1:-1]
+                var_x[:, -2] = v_pred.flatten()
+
+            net.reset_lstm_states()
+
+        # Validation
+        mu_preds = []
+        var_preds = []
+        y_val = []
+        for itera in range(num_iter):
+            ts_batch = ts_list[itera * batch_size : (itera + 1) * batch_size]
+            ts_data = {}
+            num_sample_inter = 0
+
+            for ts in ts_batch:
+                ts_data[int(ts)] = val_dtl_dict[int(ts)].dataset["value"]
+                num_sample_inter = np.maximum(
+                    len(ts_data[int(ts)][1]), num_sample_inter
+                )
+
+            x, _ = prep_x_y(0, ts_batch, ts_data, input_size, batch_size)
+            x = x.reshape(batch_size, input_size)
+            var_x = np.zeros_like(x)
+
+            for t in range(num_sample_inter):
+
+                _x, y = prep_x_y(t, ts_batch, ts_data, input_size, batch_size)
+                _x = _x.reshape(batch_size, input_size)
+                x[:, -1] = _x[:, -1]
+
+                # Feed forward
+                m_pred, v_pred = net(x.flatten(), var_x.flatten())
+                mu_preds.extend(m_pred)
+                var_preds.extend(v_pred + sigma_v**2)
+                y_val.extend(y)
+
+                # modify input
+                x[:, :-2] = x[:, 1:-1]
+                x[:, -2] = m_pred.flatten()
+                var_x[:, :-2] = var_x[:, 1:-1]
+                var_x[:, -2] = v_pred.flatten()
+
+            net.reset_lstm_states()
+
+        rmse = metric.rmse(np.array(mu_preds), np.array(y_val))
+
+        if rmse < rmse_optim and epoch > warmup_epochs:
+            rmse_optim = rmse
+            epoch_optim = epoch
+            net.save(f"saved_results/hq_100ts_g.bin")
+
+        # Progress bar
+        pbar.set_description(
+            f"Epoch {epoch + 1}/{num_epochs}| rmse: {rmse:0.4f} | sigma_v: {sigma_v:0.4f}",
+            refresh=True,
+        )
+
+        if epoch - epoch_optim > patience:
+            break
+
+    # -------------------------------------------------------------------------#
+    # Testing
+    print(f"Optimal epoch: {epoch_optim}")
+    print(f"RMSE: {rmse_optim:0.4f}")
+
+
+def prep_x_y(timestep, ts_batch, ts_data, input_size, batch_size):
+    x = np.full(input_size * batch_size, np.nan, dtype=np.float32)
+    y = np.full(batch_size, np.nan, dtype=np.float32)
+    for i, ts in enumerate(ts_batch):
+        if timestep > len(ts_data[int(ts)][1]) - 1:
+            _x = np.full(input_size, np.nan, dtype=np.float32)
+            _y = np.full(1, np.nan, dtype=np.float32)
+        else:
+            _x = ts_data[int(ts)][0][timestep]
+            _y = ts_data[int(ts)][1][timestep]
+
+        x[i * input_size : (i + 1) * input_size] = _x
+        y[i] = _y[0]
+    x = np.nan_to_num(x, nan=0.0)
+    return (x, y)
+
+
+class PredictionViz:
+    """Visualization of prediction
+    Attributes:
+        task_name: Name of the task such as autoencoder
+        data_name: Name of dataset such as Boston housing or toy example
+        figsize: Size of figure
+        fontsize: Font size for letter in the figure
+        lw: linewidth
+        ms: Marker size
+        ndiv_x: Number of divisions for x-direction
+        ndiv_y: Number of division for y-direciton
+    """
+
+    def __init__(
+        self,
+        task_name: str,
+        data_name: str,
+        figsize: tuple = (9, 8),
+        fontsize: int = 28,
+        lw: int = 3,
+        ms: int = 10,
+        ndiv_x: int = 4,
+        ndiv_y: int = 4,
+    ) -> None:
+        self.task_name = task_name
+        self.data_name = data_name
+        self.figsize = figsize
+        self.fontsize = fontsize
+        self.lw = lw
+        self.ms = ms
+        self.ndiv_x = ndiv_x
+        self.ndiv_y = ndiv_y
+
+    def load_dataset(self, file_path: str, header: bool = False) -> np.ndarray:
+        """Load dataset (*.csv)
+        Args:
+            file_path: File path to the data file
+            header: Ignore hearder ?
+
+        """
+
+        # Load image data from *.csv file
+        if header:
+            df = pd.read_csv(file_path, skiprows=1, delimiter=",", header=None)
+        else:
+            df = pd.read_csv(file_path, skiprows=0, delimiter=",", header=None)
+
+        return df[0].values
+
+    def plot_predictions(
+        self,
+        x_test: np.ndarray,
+        y_test: np.ndarray,
+        y_pred: np.ndarray,
+        sy_pred: np.ndarray,
+        std_factor: int,
+        x_train: Optional[np.ndarray] = None,
+        y_train: Optional[np.ndarray] = None,
+        sy_test: Optional[np.ndarray] = None,
+        label: str = "diag",
+        title: Optional[str] = None,
+        eq: Optional[str] = None,
+        x_eq: Optional[float] = None,
+        y_eq: Optional[float] = None,
+        time_series: bool = False,
+        save_folder: Optional[str] = None,
+    ) -> None:
+        """Compare prediciton distribution with theorical distribution
+
+        x_train: Input train data
+        y_train: Output train data
+        x_test: Input test data
+        y_test: Output test data
+        y_pred: Prediciton of network
+        sy_pred: Standard deviation of the prediction
+        std_factor: Standard deviation factor
+        sy_test: Output test's theorical standard deviation
+        label: Name of file
+        title: Figure title
+        eq: Math equation for data
+        x_eq: x-coordinate for eq
+        y_eq: y-coordinate for eq
+
+        """
+
+        # Get max and min values
+        if sy_test is not None:
+            std_y = max(sy_test)
+        else:
+            std_y = 0
+
+        if x_train is not None:
+            max_y = np.maximum(max(y_test), max(y_train)) + std_y
+            min_y = np.minimum(min(y_test), min(y_train)) - std_y
+            max_x = np.maximum(max(x_test), max(x_train))
+            min_x = np.minimum(min(x_test), min(x_train))
+        else:
+            max_y = max(y_test) + std_y
+            min_y = min(y_test) - std_y
+            max_x = max(x_test)
+            min_x = min(x_test)
+
+        # Plot figure
+        if time_series:
+            marker = ""
+            line_style = "-"
+        else:
+            marker = "o"
+            line_style = ""
+        plt.figure(figsize=self.figsize)
+        ax = plt.axes()
+        ax.set_title(title, fontsize=1.1 * self.fontsize, fontweight="bold")
+        if eq is not None:
+            ax.text(x_eq, y_eq, eq, color="k", fontsize=self.fontsize)
+        ax.plot(x_test, y_pred, "r", lw=self.lw, label=r"$\mathbb{E}[Y^{'}]$")
+        ax.plot(
+            x_test,
+            y_test,
+            "k",
+            lw=self.lw,
+            label=r"$y_{true}$",
+            marker=marker,
+            linestyle=line_style,
+        )
+
+        ax.fill_between(
+            x_test,
+            y_pred - std_factor * sy_pred,
+            y_pred + std_factor * sy_pred,
+            facecolor="red",
+            alpha=0.3,
+            label=r"$\mathbb{{E}}[Y^{{'}}]\pm{}\sigma$".format(std_factor),
+        )
+        if sy_test is not None:
+            ax.fill_between(
+                x_test,
+                y_test - std_factor * sy_test,
+                y_test + std_factor * sy_test,
+                facecolor="blue",
+                alpha=0.3,
+                label=r"$y_{{test}}\pm{}\sigma$".format(std_factor),
+            )
+        if x_train is not None:
+            ax.plot(
+                x_train,
+                y_train,
+                "b",
+                marker=marker,
+                mfc="none",
+                lw=self.lw,
+                ms=0.2 * self.ms,
+                linestyle=line_style,
+                label=r"$y_{train}$",
+            )
+
+        ax.set_xlabel(r"$x$", fontsize=self.fontsize)
+        ax.set_ylabel(r"$y$", fontsize=self.fontsize)
+        if time_series:
+            x_ticks = pd.date_range(min_x, max_x, periods=self.ndiv_x).values
+        else:
+            x_ticks = np.linspace(min_x, max_x, self.ndiv_x)
+        y_ticks = np.linspace(min_y, max_y, self.ndiv_y)
+        ax.set_yticks(y_ticks)
+        ax.set_xticks(x_ticks)
+        ax.tick_params(
+            axis="both",
+            which="both",
+            direction="inout",
+            labelsize=self.fontsize,
+        )
+        ax.legend(
+            loc="upper right",
+            edgecolor="black",
+            fontsize=1 * self.fontsize,
+            ncol=1,
+            framealpha=0.3,
+            frameon=False,
+        )
+        ax.set_ylim([min_y, max_y])
+        ax.set_xlim([min_x, max_x])
+
+        # Save figure
+        if save_folder is None:
+            plt.show()
+        else:
+            saving_path = f"saved_results/pred_{label}_{self.data_name}.png"
+            plt.savefig(saving_path, bbox_inches="tight")
+            plt.close()
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
